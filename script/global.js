9f3b0d9af9d6f174d2a36b307f2bccf6b0eb6513
Giacomo Rossettini <rossegiac@gmail.com>
2024-09-01 09:58:27 +0200

    menu

--- script/global.js
+++ script/global.js
@@ -140,3 +140,121 @@ textButton.addEventListener("click", function () {
   imageButton.style.display = "none";
   printButton.style.display = "none";
 });
+
+// Funzione per aprire il sottomenu di imageButton __________________________________________________________________
+// vado a cercare se esiste il mio elemento, e nel caso lo rimuovo
+imageButton.addEventListener("click", function () {
+  let existingForm = document.querySelector("#shirt__image");
+  if (existingForm) {
+    existingForm.remove();
+    colorButton.style.display = "";
+    textButton.style.display = "";
+    printButton.style.display = "";
+    return; // Esce dalla funzione se il form viene rimosso
+  }
+
+  let form = document.createElement("form");
+  form.id = "shirt__image";
+  form.className = "panel";
+  //______________ QUI INIZIA IL CORPO DEL FORM
+  let pFile = document.createElement("p");
+  pFile.innerText = "Carica la foto che preferisci";
+
+  // creazione file
+  let file = document.createElement("input");
+  file.type = "file";
+  file.name = "input-file";
+  file.id = "input__file";
+
+  // creazione range opacity
+  let pOpacity = document.createElement("p");
+  pOpacity.innerText = "Regola l'opacità dell'immagine";
+  let rangeOpacity = document.createElement("input");
+  rangeOpacity.type = "range";
+  rangeOpacity.name = "opacity";
+  rangeOpacity.id = "opacity";
+  rangeOpacity.min = "0";
+  rangeOpacity.max = "1";
+  rangeOpacity.step = "0.01";
+  rangeOpacity.value = "1";
+
+  // creazione range border radius
+  let pRadius = document.createElement("p");
+  pRadius.innerText = "Arrotonda i bordi";
+  let rangeRadius = document.createElement("input");
+  rangeRadius.type = "range";
+  rangeRadius.name = "borders";
+  rangeRadius.id = "borders";
+  rangeRadius.min = "0";
+  rangeRadius.max = "100";
+  rangeRadius.step = "1";
+  rangeRadius.value = "0";
+
+  // creazione range dimension
+  let pDimension = document.createElement("p");
+  pDimension.innerText = "Cambia la dimensione";
+  let rangeDimension = document.createElement("input");
+  rangeDimension.type = "range";
+  rangeDimension.name = "dimension";
+  rangeDimension.id = "dimension";
+  rangeDimension.min = "0";
+  rangeDimension.max = "100";
+  rangeDimension.step = "1";
+  rangeDimension.value = "100";
+
+  // creazione joypad
+  let pPosition = document.createElement("p");
+  pPosition.innerText = "Posiziona l'immagine lungo la maglietta";
+  let joypad = document.createElement("div");
+  joypad.className = "joypad";
+  let up = document.createElement("button");
+  up.id = "up";
+  up.className = "button";
+  up.innerHTML = `<i class="fas fa-arrow-alt-circle-up"></i>`;
+  let right = document.createElement("button");
+  right.id = "right";
+  right.className = "button";
+  right.innerHTML = `<i class="fas fa-arrow-alt-circle-right"></i>`;
+  let left = document.createElement("button");
+  left.id = "left";
+  left.className = "button";
+  left.innerHTML = `<i class="fas fa-arrow-alt-circle-left">`;
+  let down = document.createElement("button");
+  down.id = "down";
+  down.className = "button";
+  down.innerHTML = `<i class="fas fa-arrow-alt-circle-down"></i>`;
+
+  //________________DA QUI INIZIA IL DIV DEI PULSANTI _______________________
+  let div = document.createElement("div");
+
+  let submit = document.createElement("button");
+  submit.type = "submit";
+  submit.className = "button";
+  submit.innerText = "Conferma";
+
+  let reset = document.createElement("input");
+  reset.id = "reset";
+  reset.type = "reset";
+  reset.className = "button";
+  reset.value = "Reset";
+
+  div.append(submit, reset);
+  joypad.append(up, left, down, right);
+  form.append(pFile, file, pOpacity, rangeOpacity, pRadius, rangeRadius, pDimension, rangeDimension, pPosition, joypad, div); //appendo nel FORM
+  ul.insertBefore(form, imageButton.nextElementSibling); // metto il form dentro ul subito dopo il li scelto
+
+  // Gestione hover per textbutton
+  if (form) {
+    imageButton.addEventListener("mouseover", function () {
+      imageButton.style.backgroundColor = "white";
+    });
+
+    textButton.addEventListener("mouseout", function () {
+      imageButton.style.backgroundColor = "rgb(55, 55, 55)";
+    });
+  }
+  // altri pulsanti nascosti
+  colorButton.style.display = "none";
+  textButton.style.display = "none";
+  printButton.style.display = "none";
+});
